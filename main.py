a8574baf3b54ade42c5e60d3de4a883bc1bbc4fa
ThGox-06 <thgimenez06@gmail.com>
2023-05-28 21:02:49 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from module_0 import mod_0
+
+# This generates de password
+mod_0()
+print(mod_0())
---
+++ module_0.py
@@ -0,0 +1,7 @@
+import random
+
+
+def mod_0():
+    rand_num = random.randint(97, 122)
+    rand_num = chr(rand_num)
+    return rand_num, 1
